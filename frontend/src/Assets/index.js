eb0e716b107ce5df25cf36b98ba9d19d07852bb3
mza-codes <muzofficial90@gmail.com>
2022-12-15 12:03:54 +0000

    Functional Editor,Added States,Splitted Components,Removed BG Image

--- frontend/src/Assets/index.js
+++ frontend/src/Assets/index.js
@@ -1,3 +1,9 @@
 import background from './bg-small.jpg';
 
 export const bg = background;
+
+export const hooker = (field, hook) => {
+    return hook((state) => state[field]);
+};
+
+export const RTFTemplate = `<h2 style="text-align: justify">Robert Conels</h2><p style="text-align: justify"><strong>Place: NewYork,HIMA</strong></p><p style="text-align: justify"><strong>Age: 45</strong></p><p style="text-align: justify"><strong>Diagnosis: oTrefRt</strong></p><p style="text-align: justify"><strong>Last Checkup: 04/11/2022</strong></p><p style="text-align: justify"><strong>Status: 4</strong></p><h4 style="text-align: justify"><strong>Primary Stage</strong></h4><ul><li><p style="text-align: justify"><strong>Diagnosed with TFRT</strong></p></li><li><p style="text-align: justify"><strong>Started Treatment on 4th May</strong></p></li><li><p style="text-align: justify"><strong>Statuscode 6</strong></p></li></ul><h4 style="text-align: justify"><strong>Secondary Stage</strong></h4><ul><li><p style="text-align: justify"><strong>Major progress</strong></p></li><li><p style="text-align: justify"><strong>Stauscode 2</strong></p></li><li><p style="text-align: justify"><strong>Stage 5 Entered</strong></p></li><li><p style="text-align: justify"><strong>Decreased Medication </strong></p></li></ul><h4 style="text-align: justify"><strong>Tertiary Stage</strong></h4><ul><li><p style="text-align: justify"><strong>Improved Health</strong></p></li><li><p style="text-align: justify"><strong>Statuscodes cleared</strong></p></li></ul><p style="text-align: justify"></p><hr><blockquote><p style="text-align: center">Discharged and cleared Case ! </p><p style="text-align: center">© 2022 <strong>medic-log</strong></p></blockquote>`;
--- frontend/src/Pages/AddRecord.jsx
+++ frontend/src/Pages/AddRecord.jsx
@@ -1,7 +1,6 @@
 import { lazy, Suspense } from "react";
-import { bg } from "../Assets";
-import AddData from "../Components/Forms/AddData";
-import EditorData from "../Components/Forms/EditorData";
+import AddDataForm from "../Components/Forms/AddData";
+// import { bg } from "../Assets";
 import Loader from "../Components/Loader/Loader";
 
 const RTF = lazy(() => import("../Components/Forms/RichTextEditor.jsx"));
@@ -9,17 +8,18 @@ const RTF = lazy(() => import("../Components/Forms/RichTextEditor.jsx"));
 const AddRecord = () => {
 
     return (
-        <main style={{ backgroundImage: `url(${bg})` }} className='w-full min-h-[94vh] bg-cover relative'>
+        <main 
+        // style={{ backgroundImage: `url(${bg})` }} 
+        className='w-full min-h-[94vh] relative bg-gradient-to-r from-teal-50 via-emerald-100 to-teal-100'>
             <section className="w-full py-4 bg-black bg-opacity-5 min-h-[94vh]">
                 <h2 className="text-3xl font-semibold text-center my-3">Add Patient Data</h2>
-                <AddData />
+                <AddDataForm />
                 <Suspense fallback={<Loader />}>
                         <div className="max-w-[100vw] p-2 flex flex-col items-center justify-center">
                             <h1 className="text-4xl my-2 text-center">Add Patient Log</h1>
-                            <RTF className="lg:max-w-[800px]" />
+                            <RTF className="lg:max-w-[800px] rtfEditor" />
                         </div>
                 </Suspense>
-                {/* <EditorData /> */}
             </section>
         </main>
     );
